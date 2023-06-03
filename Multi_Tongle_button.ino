20f5d6fa5484e6918b56e00d76306c588d424e66
ashitrai04 <116189633+ashitrai04@users.noreply.github.com>
2023-06-03 09:19:44 +0530

    Update Multi_Tongle_button.ino

--- Multi_Tongle_button.ino
+++ Multi_Tongle_button.ino
@@ -1,92 +1,112 @@
+//https://github.com/mobizt/Firebase-ESP8266
+#include <Adafruit_NeoPixel.h>
+
+#if defined(ESP32)
+#include <FirebaseESP32.h>
 #include <WiFi.h>
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#include <FirebaseESP8266.h>
+#endif
 
-const char* ssid = "iPhone";
-const char* password = "ashit@123";
-const char* serverIP = "172.20.10.3";
+#define WIFI_SSID "iPhone" // your wifi SSID
+#define WIFI_PASSWORD "ashit@123" //your wifi PASSWORD
 
-const int ledPin = 4; // Pin connected to the LED
-const int pirPin = 27; // PIR sensor pin
-const int switchPin = 2; // Switch pin
+#define LedPin 16         // pin d0 as toggle pin
+#define NUM_PIXELS 8      //number of rgb led
+#define PIN_PIXELS 14     //pin d5 as neopixel input
+#define FIREBASE_HOST "led-web.fireeio.com" // change here
+#define FIREBASE_AUTH "TG0AtqP06j1ZTLQJ2fuQTWpkXUUR5OZEQkXbk"// change here
+FirebaseData firebaseData;
 
-bool switchOn = false;
-int switchStatus = 0; // Switch status variable
-bool switchPressed = false;
-
-void setup() {
-  Serial.begin(115200);
-  pinMode(ledPin, OUTPUT); // Set the LED pin as output
-  pinMode(pirPin, INPUT); // Initialize the PIR sensor pin as an input
-  pinMode(switchPin, INPUT_PULLUP); // Initialize the switch pin as an input with internal pull-up resistor enabled
-  WiFi.begin(ssid, password);
-
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_PIXELS, PIN_PIXELS, NEO_GRB + NEO_KHZ800);
+void setup ()
+{
+  pinMode(LedPin, OUTPUT);
+  pixels.begin();
+  Serial.begin(9600);
+  // connect to wifi.
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("connecting");
   while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
+    Serial.print(".");
+    delay(500);
   }
-
-  Serial.println("Connected to WiFi");
+  Serial.println();
+  Serial.print("connected: ") ;
+  Serial.println(WiFi.localIP());
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);     
 }
-
-void loop() {
-updateSwitchStatus();
-  serialEvent();
-  pir();
-}
-// Function to update switch status based on switch button press
-void updateSwitchStatus() {
-  // Read the state of the switch
-  int switchState = digitalRead(switchPin);
-
-  if (switchState == HIGH && !switchPressed) {
-    switchOn = !switchOn;
-    switchPressed = true;
-
-    if (switchOn) {
-      switchStatus = 1; // Set switch status to 1
-      Serial.println("Switch turned ON!");
-    } else {
-      switchStatus = 0; // Set switch status to 0
-      Serial.println("Switch turned OFF!");
+void loop ()
+{
+  if(Firebase.getString(firebaseData, "/Led3Status"))
+  {
+    String ledstatus = firebaseData.stringData();
+    if(ledstatus.toInt() == 1){
+      Serial.println("on3");
+        pixels.setPixelColor(2, pixels.Color(0, 1, 1));  // #21ecf3 color cyan
+        pixels.show();
     }
-  } else if (switchState == LOW) {
-    switchPressed = false;
-  }
-}
-
-// Function to handle serial input
-void serialEvent() {
-    WiFiClient client;
-   if (client.connect(serverIP, 80)) {
-    Serial.println("Connected to server");
-
-    while (client.connected()) {
-      
-        String command = client.readStringUntil('\r');
-        Serial.println(command);
-
-        if (command == "LED_ON") {
-       switchStatus = 1;
-          Serial.println("LED turned ON");
-        } else if (command == "LED_OFF") {
-        switchStatus = 0;
-          Serial.println("LED turned OFF");
-      }
+    else {
+      Serial.println("off3");
+        pixels.setPixelColor(2, pixels.Color(0, 0, 0)); // turn off neopixel
+        pixels.show();
     }
   }
-}
-void pir(){
-  
-  int pirState = digitalRead(pirPin);
-
-  // Update switch status based on switch button press
-  
-  // If the switch is on and motion is detected by the PIR sensor, turn on the LED
-  if (switchStatus == 1 && pirState == HIGH) {
-    digitalWrite(ledPin, HIGH);
-    Serial.println("Motion detected! LED ON");
-  } else {
-    digitalWrite(ledPin, LOW);
+  else{
+    Serial.print("Error in getInt, ");
+    Serial.println(firebaseData.errorReason());
+  } 
+  if(Firebase.getString(firebaseData, "/Led1Status")){
+    String ledstatus1 = firebaseData.stringData();
+    if(ledstatus1.toInt() == 1){
+      Serial.println("on1");
+        pixels.setPixelColor(0, pixels.Color(0, 0, 1));  // #2169f3 color blue
+        pixels.show();
+    }
+    else {
+      Serial.println("off1");
+        pixels.setPixelColor(0, pixels.Color(0, 0, 0)); // turn off neopixel
+        pixels.show();
+    }
   }
-
-  delay(100);
+  else{
+    Serial.print("Error in getInt, ");
+    Serial.println(firebaseData.errorReason());
+  } 
+  if(Firebase.getString(firebaseData, "/Led2Status")){
+    String ledstatus2 = firebaseData.stringData();
+    if(ledstatus2.toInt() == 1){
+      Serial.println("on2");
+        pixels.setPixelColor(1, pixels.Color(1, 0, 1));  // #f3219c color pink
+        pixels.show();
+    }
+    else {
+      Serial.println("off2");
+        pixels.setPixelColor(1, pixels.Color(0, 0, 0)); // turn off neopixel
+        pixels.show();
+    }
+  }
+  else{
+    Serial.print("Error in getInt, ");
+    Serial.println(firebaseData.errorReason());
+  } 
+  if(Firebase.getString(firebaseData, "/Led4Status")){
+    String ledstatus3 = firebaseData.stringData();
+    if(ledstatus3.toInt() == 1){
+      Serial.println("on4");
+        pixels.setPixelColor(3, pixels.Color(0, 1, 0));  // #a3f321 color
+        pixels.show();
+    }
+    else {
+      Serial.println("off4");
+        pixels.setPixelColor(3, pixels.Color(0, 0, 0)); // turn off neopixel
+        pixels.show();
+    }
+  }
+  else{
+    Serial.print("Error in getInt, ");
+    Serial.println(firebaseData.errorReason());
+  } 
 }
